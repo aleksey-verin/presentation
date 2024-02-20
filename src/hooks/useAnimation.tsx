17593e730dd5ead2c53170237598f30bada7cd21
Aleksey Verin <verevaa@gmail.com>
2024-02-20 22:07:25 +0300

    fix: remove unnecessary imports

--- src/hooks/useAnimation.tsx
+++ src/hooks/useAnimation.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useRef } from 'react'
+import { useEffect } from 'react'
 
 const NUM_CONFETTI = 350
 const COLORS = [
--- src/pages/step13/page.tsx
+++ src/pages/step13/page.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef, useState } from 'react'
+import { useEffect, useRef } from 'react'
 import { useNavigate } from 'react-router-dom'
 import { Button } from '../../components/button'
 import styles from './page.module.css'
